9ca218b72f65a9e6086ab3fb592a7a5eecafbaa0
wahyuandhi <wakay.98@gmail.com>
2021-08-23 16:59:25 +0700

    Add default value parameter

--- bin/konversi_suhu.dart
+++ bin/konversi_suhu.dart
@@ -7,8 +7,8 @@ void main() {
   var kelvin = (fahrenheit - 32) * 5 / 9 + 273.15;
   var reamur = (fahrenheit - 32) * 4 / 9;
   result(type1: 'fahrenheit', num1: fahrenheit, type2: 'celsius', num2: celcius);
-  result(type1: 'fahrenheit', num1: fahrenheit, type2: 'kelvin', num2: kelvin);
-  result(type1: 'fahrenheit', num1: fahrenheit, type2: 'reamur', num2: reamur);
+  result(num1: fahrenheit, type2: 'kelvin', num2: kelvin);
+  result(num1: fahrenheit, type2: 'reamur', num2: reamur);
 }
 
-void result({required String type1, required num num1, required String type2, required num num2}) => print('$num1 derajat $type1 = $num2 derajat $type2');
+void result({String type1 ='fahrenheit', required num num1, required String type2, num? num2}) => print('$num1 derajat $type1 = $num2 derajat $type2');
